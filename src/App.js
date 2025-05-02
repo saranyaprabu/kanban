aa6bf9ef8beeff534585ead3d2bc78faa4a61e4f
saranyaprabu <saranyaprabu1106@gmail.com>
2025-05-02 18:13:54 +0530

    Updated route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/" element={<Dashboard/>}></Route>
       </Routes>
     </Router>
   );
